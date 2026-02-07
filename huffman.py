38223ec3ee5de1b3aa2367ae4a9f5a15f600282d
DaveMatNat <davidmatthewnathanson@gmail.com>
2026-02-07 14:42:28 -0500

    Added test cases

--- huffman.py
+++ huffman.py
@@ -1,5 +1,5 @@
-# from collections import defaultdict
 import heapq
+import sys
 from itertools import count
 
 class Node:
@@ -67,9 +67,15 @@ class HuffmanCoding:
         return new_data
 
     def generate_encoding(self) -> dict:
-        # Traverse Tree in Pre-Order
         root = self.huffman_tree
         encodings = dict()
+
+        # Edge case: only one unique symbol
+        if root.left_child is None and root.right_child is None:
+            encodings[root.symbol] = "0"
+            return encodings
+
+        # Traverse Tree in Pre-Order
         def preOrder(node: Node, res=''):
         # Base cases
             # if current Node is None
@@ -95,6 +101,11 @@ class HuffmanCoding:
         output = ""
         curr_node = root
 
+        # Edge case: only one unique symbol
+        if root.left_child is None and root.right_child is None:
+            # each bit corresponds to that symbol (with the "0" convention above)
+            return root.symbol * len(self.new_data)
+
         for b in self.new_data:
             if b == '0':
                 curr_node = curr_node.left_child
@@ -103,23 +114,37 @@ class HuffmanCoding:
             if not curr_node.left_child and not curr_node.right_child:
                 output += curr_node.symbol
                 curr_node = root
-                
+
         return output
+    
+    def print_symbol_frequency(self, decreasing_order=True) -> None:
+        sorted_freq = sorted(huffman_coding.frequency.items(), key=lambda item: item[1], reverse=decreasing_order)
+        print(f"Unique Chars: {len(sorted_freq)}\n")
+        for sym, freq in sorted_freq:
+            if sym != "\n":
+                print(f"{freq} : \'{sym}\'")
+            else:
+                print(f"{freq} : \'newline\'")
 
     def __str__(self):
+        length = 100
         old_bits = len(self.original_data) * 8
         new_bits = len(self.new_data)
 
         compression_ratio = new_bits / old_bits if old_bits else 0
         space_saving = 1 - compression_ratio
-
-        print(f"Old Data: ({old_bits} bits)\n\t{self.original_data}\n")
-        print(f"New Data: ({new_bits} bits)\n\t{self.new_data}\n")
+        print(f"Old Data: ({old_bits} bits)\n{self.original_data[:length]}{'...' if len(self.original_data) >= length else ''}\n")
+        print(f"New Data: ({new_bits} bits)\n{self.new_data[:length]}{'...' if len(self.new_data) >= length else ''}\n")
         print(f"Compression ratio: {compression_ratio:.4f}")
         print(f"Space saved: {space_saving*100:.2f}%")
         return ""
+    
 if __name__ == '__main__':
-    file_path = 'data.txt'
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+        file_path = str(path)
+    else:
+        file_path = 'data.txt'
 
     data = ""
 
@@ -136,8 +161,6 @@ if __name__ == '__main__':
 
     print(huffman_coding)
 
-    # for sym, freq in huffman_coding.frequency.items():
-    #     print(f"{freq} : {sym}")
+    huffman_coding.print_symbol_frequency()
 
-
-    print(huffman_coding.generate_decode())
+    print(huffman_coding.generate_decode() == huffman_coding.original_data)
